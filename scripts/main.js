2b32eda4a1fc171413bfc205e5819bf8ccbfdf7d
Kelly de Vries <kdv@me.com>
2015-11-19 16:00:59 -0800

    add sample fish

--- scripts/main.js
+++ scripts/main.js
@@ -28,6 +28,11 @@ var App = React.createClass({
     // allows for speedier virtual DOM comparison
     this.setState({ fishes : this.state.fishes });
   },
+  loadSamples : function() {
+    this.setState({
+      fishes : require('./sample-fishes')
+    });
+  },
   render : function() {
     return (
       <div className="catch-of-the-day">
@@ -35,7 +40,8 @@ var App = React.createClass({
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish}/>
+        <Inventory  addFish={this.addFish}
+                    loadSamples={this.loadSamples}/>
       </div>
     )
   }
@@ -119,6 +125,7 @@ var Inventory = React.createClass({
       <div>
         <h2>Inventory</h2>
       <AddFishForm {...this.props} />
+      <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
     )
   }
